99a427d3748f82a6253ed972770335798a00f751
Sahaj Singh <sahajsingh2313@gmail.com>
2021-06-23 17:08:48 -0700

    More GUI changes

--- window.py
+++ window.py
@@ -2,10 +2,6 @@ from tkinter import *
 import tkinter as tk
 from tkinter import filedialog, messagebox
 
-# import sys, os
-# path = getattr(sys, '_MEIPASS', os.getcwd())
-# os.chdir(path)
-
 
 def select_path(event):
     global output_path
@@ -15,9 +11,25 @@ def select_path(event):
     entry0.insert(0, output_path)
 
 
+def select_path_destination(event):
+    global output_path2
+
+    output_path2 = filedialog.askdirectory()
+    entry3.delete(0, END)
+    entry3.insert(0, output_path)
+
+
 def btn_clicked():
     Value = entry0.get()
     file = open('path.txt', 'a')
+    file.seek(0)
+    file.write(str(Value) + '\n')
+    print(Value)
+
+
+def btn_clicked():
+    Value = entry3.get()
+    file = open('path_destination.txt', 'a')
     file.seek(0)
     file.write(str(Value) + '\n')
     print(Value)
@@ -60,65 +72,88 @@ def minute_decrement():
 
 
 def time_am():
-    file = open('time.txt', 'a')
-    file.seek(0)
-    print_hour = entry1.get()
-    print_minute = entry2.get()
-    if int(print_hour) == 0 and int(print_minute) == 0:
-        print_time = "00:00 AM"
-        messagebox.showinfo('Time', str(print_time))
-        print(print_time)
-        file.write(str(print_hour) + ":" + str(print_minute) + "AM" + "\n")
-        file.close()
-    elif int(print_hour) < 10 and int(print_minute) < 10:
-        print_time = "0" + str(print_hour) + ":0" + str(print_minute) + " AM"
-        messagebox.showinfo('Time', str(print_time))
-        print(print_time)
-        file.write(str(print_hour) + ":" + str(print_minute) + "AM" + "\n")
-        file.close()
-    else:
-        print_time = str(print_hour) + ":" + str(print_minute) + " AM"
-        messagebox.showinfo('Time', str(print_time))
-        print(print_time)
-        file.write(str(print_hour) + ":" + str(print_minute) + "AM" + "\n")
-        file.close()
+    global time_set_am
+    time_set_am = True
+    print(time_set_am)
 
 
 def time_pm():
-    file = open('time.txt', 'a')
-    file.seek(0)
-    print_hour = int(entry1.get())
-    print_minute = entry2.get()
-    if int(print_hour) == 0 and int(print_minute) == 0:
-        print_time = "00:00 PM"
-        messagebox.showinfo('Time', str(print_time))
-        print(print_time)
-        file.write(str(print_hour) + ":" + str(print_minute) + "PM" + "\n")
-        file.close()
-    elif int(print_hour) < 10 and int(print_minute) < 10:
-        print_time = "0" + str(print_hour) + ":0" + str(print_minute) + " PM"
-        messagebox.showinfo('Time', str(print_time))
-        print(print_time)
-        file.write(str(print_hour) + ":" + str(print_minute) + "PM" + "\n")
-        file.close()
-    else:
-        print_time = str(print_hour) + ":" + str(print_minute) + " PM"
-        messagebox.showinfo('Time', str(print_time))
-        print(print_time)
-        file.write(str(print_hour) + ":" + str(print_minute) + "PM" + "\n")
-        file.close()
+    global time_set_pm
+    time_set_pm = True
+    print(time_set_pm)
+
+
+def time_select():
+    if time_set_am:
+        file = open('time.txt', 'a')
+        file.seek(0)
+        print_hour = entry1.get()
+        print_minute = entry2.get()
+        if int(print_hour) == 0 and int(print_minute) == 0:
+            print_time = "00:00 AM"
+            messagebox.showinfo('Time', str(print_time))
+            print(print_time)
+            file.write(str(print_hour) + ":" + str(print_minute) + "AM" + "\n")
+            file.close()
+        elif int(print_hour) < 10 and int(print_minute) < 10:
+            print_time = "0" + str(print_hour) + ":0" + str(print_minute) + " AM"
+            messagebox.showinfo('Time', str(print_time))
+            print(print_time)
+            file.write(str(print_hour) + ":" + str(print_minute) + "AM" + "\n")
+            file.close()
+        else:
+            print_time = str(print_hour) + ":" + str(print_minute) + " AM"
+            messagebox.showinfo('Time', str(print_time))
+            print(print_time)
+            file.write(str(print_hour) + ":" + str(print_minute) + "AM" + "\n")
+            file.close()
+    elif time_set_pm:
+        file = open('time.txt', 'a')
+        file.seek(0)
+        print_hour = int(entry1.get())
+        print_minute = entry2.get()
+        if int(print_hour) == 0 and int(print_minute) == 0:
+            print_time = "00:00 PM"
+            messagebox.showinfo('Time', str(print_time))
+            print(print_time)
+            file.write(str(print_hour) + ":" + str(print_minute) + "PM" + "\n")
+            file.close()
+        elif int(print_hour) < 10 and int(print_minute) < 10:
+            print_time = "0" + str(print_hour) + ":0" + str(print_minute) + " PM"
+            messagebox.showinfo('Time', str(print_time))
+            print(print_time)
+            file.write(str(print_hour) + ":" + str(print_minute) + "PM" + "\n")
+            file.close()
+        else:
+            print_time = str(print_hour) + ":" + str(print_minute) + " PM"
+            messagebox.showinfo('Time', str(print_time))
+            print(print_time)
+            file.write(str(print_hour) + ":" + str(print_minute) + "PM" + "\n")
+            file.close()
 
 
 def clear_func():
     text.set("")
 
 
+def clear_func2():
+    text2.set("")
+
+
+def time_clear():
+    hour.set("")
+    minute.set("")
+
+
 window = Tk()
 
-global output_path, text, hour, minute
+global output_path, output_path2, text, text2, hour, minute, time_set_am, time_set_pm
 text = tk.StringVar()
+text2 = tk.StringVar()
 hour = tk.IntVar()
 minute = tk.IntVar()
+time_set_am = tk.BooleanVar()
+time_set_pm = tk.BooleanVar()
 
 window.geometry("1440x1024")
 window.configure(bg="#009bff")
@@ -144,20 +179,38 @@ canvas.create_text(
     font=("RobotoCondensed-Regular", int(96.0)))
 
 canvas.create_text(
-    233.5, 248.0,
+    227.5, 239.0,
     text="This is is an example file",
     fill="#000000",
     font=("RobotoCondensed-Regular", int(24.0)))
 
 canvas.create_text(
-    233.5, 276.0,
+    227.5, 267.0,
     text="where we will be testing out our GUI",
     fill="#000000",
     font=("RobotoCondensed-Regular", int(24.0)))
 
 canvas.create_text(
-    233.5, 304.0,
+    227.5, 295.0,
     text="Feel free to play around!",
+    fill="#000000",
+    font=("RobotoCondensed-Regular", int(24.0)))
+
+canvas.create_text(
+    227.5, 354.0,
+    text="Source Folder",
+    fill="#000000",
+    font=("RobotoCondensed-Regular", int(24.0)))
+
+canvas.create_text(
+    227.5, 455.0,
+    text="Storage Folder",
+    fill="#000000",
+    font=("RobotoCondensed-Regular", int(24.0)))
+
+canvas.create_text(
+    227.5, 560.0,
+    text="Select Time",
     fill="#000000",
     font=("RobotoCondensed-Regular", int(24.0)))
 
@@ -170,7 +223,7 @@ b0 = Button(
     relief="flat")
 
 b0.place(
-    x=720, y=349,
+    x=714, y=380,
     width=160,
     height=30)
 
@@ -183,7 +236,7 @@ b1 = Button(
     relief="flat")
 
 b1.place(
-    x=891, y=349,
+    x=885, y=380,
     width=160,
     height=30)
 
@@ -196,7 +249,7 @@ b2 = Button(
     relief="flat")
 
 b2.place(
-    x=155, y=396,
+    x=149, y=581,
     width=74,
     height=18)
 
@@ -209,7 +262,7 @@ b3 = Button(
     relief="flat")
 
 b3.place(
-    x=155, y=416,
+    x=149, y=601,
     width=74,
     height=18)
 
@@ -222,7 +275,7 @@ b4 = Button(
     relief="flat")
 
 b4.place(
-    x=338, y=396,
+    x=332, y=581,
     width=74,
     height=18)
 
@@ -235,7 +288,7 @@ b5 = Button(
     relief="flat")
 
 b5.place(
-    x=338, y=416,
+    x=332, y=601,
     width=74,
     height=18)
 
@@ -248,7 +301,7 @@ b6 = Button(
     relief="flat")
 
 b6.place(
-    x=421, y=396,
+    x=415, y=581,
     width=74,
     height=18)
 
@@ -261,13 +314,13 @@ b7 = Button(
     relief="flat")
 
 b7.place(
-    x=421, y=416,
+    x=415, y=601,
     width=74,
     height=18)
 
 entry0_img = PhotoImage(file=f"img_textBox0.png")
 entry0_bg = canvas.create_image(
-    377.0, 363.5,
+    371.0, 394.5,
     image=entry0_img)
 
 entry0 = Entry(
@@ -277,7 +330,7 @@ entry0 = Entry(
     textvariable=text)
 
 entry0.place(
-    x=180.0, y=346,
+    x=176.0, y=377,
     width=510.0,
     height=37)
 
@@ -285,7 +338,7 @@ entry0.bind("<1>", select_path)
 
 entry1_img = PhotoImage(file=f"img_textBox1.png")
 entry1_bg = canvas.create_image(
-    102.0, 415.5,
+    96.0, 600.5,
     image=entry1_img)
 
 entry1 = Entry(
@@ -295,13 +348,13 @@ entry1 = Entry(
     textvariable=hour)
 
 entry1.place(
-    x=67.0, y=396,
+    x=61.0, y=581,
     width=70.0,
     height=37)
 
 entry2_img = PhotoImage(file=f"img_textBox2.png")
 entry2_bg = canvas.create_image(
-    284.0, 415.5,
+    278.0, 600.5,
     image=entry2_img)
 
 entry2 = Entry(
@@ -311,14 +364,91 @@ entry2 = Entry(
     textvariable=minute)
 
 entry2.place(
-    x=249.0, y=396,
+    x=243.0, y=581,
     width=70.0,
     height=37)
 
 canvas.create_text(
-    113.0, 364.0,
+    107.0, 395.0,
     text="Input Path",
     fill="#ffffff",
     font=("RobotoCondensed-Regular", int(24.0)))
 
+img8 = PhotoImage(file=f"img8.png")
+b8 = Button(
+    image=img8,
+    borderwidth=0,
+    highlightthickness=0,
+    command=btn_clicked,
+    relief="flat")
+
+b8.place(
+    x=714, y=484,
+    width=160,
+    height=30)
+
+img9 = PhotoImage(file=f"img9.png")
+b9 = Button(
+    image=img9,
+    borderwidth=0,
+    highlightthickness=0,
+    command=clear_func2,
+    relief="flat")
+
+b9.place(
+    x=885, y=484,
+    width=160,
+    height=30)
+
+img10 = PhotoImage(file=f"img10.png")
+b10 = Button(
+    image=img10,
+    borderwidth=0,
+    highlightthickness=0,
+    command=time_select,
+    relief="flat")
+
+b10.place(
+    x=514, y=587,
+    width=160,
+    height=30)
+
+img11 = PhotoImage(file=f"img11.png")
+b11 = Button(
+    image=img11,
+    borderwidth=0,
+    highlightthickness=0,
+    command=time_clear,
+    relief="flat")
+
+b11.place(
+    x=685, y=587,
+    width=160,
+    height=30)
+
+entry3_img = PhotoImage(file=f"img_textBox3.png")
+entry3_bg = canvas.create_image(
+    371.0, 498.5,
+    image=entry3_img)
+
+entry3 = Entry(
+    bd=0,
+    bg="#425b6b",
+    highlightthickness=0,
+    textvariable=text2)
+
+entry3.place(
+    x=176.0, y=481,
+    width=510.0,
+    height=37)
+
+entry3.bind("<1>", select_path_destination)
+
+canvas.create_text(
+    107.0, 499.0,
+    text="Input Path",
+    fill="#ffffff",
+    font=("RobotoCondensed-Regular", int(24.0)))
+
+# window.resizable(False, False)
 window.mainloop()
