dbad8f8753018e8c813a5a5086308bb425f0d5f3
John <gtom_1984@hotmail.ca>
2021-07-25 22:08:51 -0700

    Add ReadMe.md
    - How to run the UI

--- ui/renderer.js
+++ ui/renderer.js
@@ -3,10 +3,10 @@
 button.addEventListener('click', async ()=> {
     let data = ["calc.py", input.value];
     // window.api.send("toMain", data);
-    const newData = await window.api.invoke("toMain", data);
-    result.textContent = newData;
+    await window.api.invoke("toMain", data).then(receivedData => {
+        result.textContent = receivedData;
+    });
 });
 
-// 
 button.dispatchEvent(new Event('click'));
 
