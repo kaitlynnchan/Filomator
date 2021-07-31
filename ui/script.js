b106d4e1f8616c18c540439b53ff3cb0f89fd21c
kaitlynnchan <kaitlynnchan@outlook.com>
2021-07-31 13:01:18 -0700

    Create Week UI
    Simple implementation of the days of the week.

---
+++ ui/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+
+    // day selected
+    $(".week .day").on("click", function(){
+        $(this).toggleClass("selected");
+        console.log("toggled");
+    });
+
+});
+
