507f6734ab99f5c6e60731dcaa14533ed50ba9eb
kaitlynnchan <kaitlynnchan@outlook.com>
2022-04-28 18:05:18 -0700

    Update UI
    Added start time. Updated new task button. Update
    info in detailsbox

--- ui/script.js
+++ ui/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    fillTime();
 
     // day selected
     $(".week .day").on("click", function(){
@@ -6,4 +7,29 @@ $(document).ready(function(){
         console.log("toggled");
     });
 
+    // AM vs PM selected
+    $(".details .twelve-hour-clock button").on("click", function(){
+        $(".details .twelve-hour-clock button").each(function(){
+            $(this).toggleClass("selected");
+        });
+        console.log("toggled");
+    });
 });
+
+function fillTime(){
+    // fill hours selection dropdown
+    for(let i = 1; i <= 12; i++){
+        var option = "<option>" + i + "</option>";
+        $(".details .time .hour").append(option);
+    }
+
+    // fill hours selection dropdown
+    for(let i = 0; i <= 59; i++){
+        if(i < 10){
+            var option = "<option>0" + i + "</option>";
+        } else{
+            var option = "<option>" + i + "</option>";
+        }
+        $(".details .time .mins").append(option);
+    }
+}
