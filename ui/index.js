a0c6b28416b990e5a641b964332f13d544879603
kaitlynnchan <kaitlynnchan@outlook.com>
2022-04-30 12:24:12 -0700

    Add maximize window

--- ui/index.js
+++ ui/index.js
@@ -17,6 +17,7 @@ function createWindow() {
             preload: path.join(__dirname, 'preload.js')
         }
     })
+    win.maximize();
 
     // Loading html file
     win.loadFile('index.html');
